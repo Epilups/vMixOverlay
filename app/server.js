88adc53bcb4af4c37bad6c012a1f62088567e19e
Epilups <emils10.pilups@gmail.com>
2026-09-19 13:05:52 +0300

    add new font and change font sizes

--- app/server.js
+++ app/server.js
@@ -276,7 +276,7 @@ function api(pathname, q) {
 
 /* ---------- static + routing ---------- */
 
-const MIME = { '.html': 'text/html; charset=utf-8', '.js': 'text/javascript', '.css': 'text/css', '.json': 'application/json', '.png': 'image/png', '.jpg': 'image/jpeg', '.svg': 'image/svg+xml', '.woff2': 'font/woff2' };
+const MIME = { '.html': 'text/html; charset=utf-8', '.js': 'text/javascript', '.css': 'text/css', '.json': 'application/json', '.png': 'image/png', '.jpg': 'image/jpeg', '.svg': 'image/svg+xml', '.woff2': 'font/woff2', '.ttf': 'font/ttf' };
 
 function serveStatic(res, rel) {
   const file = path.join(ROOT, 'public', rel.replace(/^\/+/, ''));
